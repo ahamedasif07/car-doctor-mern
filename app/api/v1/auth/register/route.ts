2efc0d8743ca2ade2b1dd2dce947d59d62ebf49d
ahamedasif07 <rxasif31@gmail.com>
2026-08-24 08:10:13 +0600

    feat: implement register API route for user authentication

--- app/api/v1/auth/register/route.ts
+++ app/api/v1/auth/register/route.ts
@@ -1,10 +1,11 @@
 import AuthController from "@/controllers/auth.controller";
 
+// get registers
+export async function GET() {
+  return AuthController.handleGetRegister();
+}
 // POST /api/v1/auth/register
 export async function POST(request: Request) {
   return AuthController.handleRegister(request);
 }
-// get registers
-export async function GET(request: Request) {
-  return AuthController.handleGetRegister(request);
-}
+
